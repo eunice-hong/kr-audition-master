83c578475db9e739659d7fe6c7d273385267301e
Eunice Hong <eunjin.hong@pbagel.com>
2025-02-25 14:20:21 +0900

    feat: reset record on main screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
+	import { page } from '$app/stores';
+	import { recordStore } from '$lib/index';
 	import '../app.css';
 	let { children } = $props();
+
+	$effect(() => {
+		if ($page.url.pathname === '/') {
+			recordStore.set(0);
+		}
+	});
 </script>
 
 {@render children()}
